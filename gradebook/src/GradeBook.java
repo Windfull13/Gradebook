a4516543535ecd2e5456b235592fbe9d10728a3b
Windfull13 <145623775+Windfull13@users.noreply.github.com>
2024-04-17 12:42:06 -0500

    Delete gradebook directory

--- gradebook/src/GradeBook.java
+++
@@ -1,9 +0,0 @@
-public class GradeBook {
-    public static void main(String[] args) {
-        GradebookTest gradeBook1 = new GradebookTest("CS101 Introduction to Java Programming");
-        GradebookTest gradeBook2 = new GradebookTest("CS102 Data Structures in Java");
-
-        gradeBook1.displayMessage();
-        gradeBook2.displayMessage();
-}
-}
